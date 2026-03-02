2311b544bb15ab004083af327cfe58d41f712258
mgtownsend21-creator <mgtownsend21@gmail.com>
2026-03-02 16:13:36 -0500

    test

--- waivetest/ContentView.swift
+++ waivetest/ContentView.swift
@@ -3,7 +3,7 @@ import CoreHaptics
 
 struct ContentView: View {
     
-    // Stores the Core Haptics engine that powers vibration feedback
+    // Stores the Core Haptics engine that powers vibration feedback ur mom
     @State private var engine: CHHapticEngine?
     
     // Stores the haptic player used to start and stop the vibration pattern
